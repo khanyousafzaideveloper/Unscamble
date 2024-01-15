077a1abdf2078be36aff3b40cbdb412c369042fa
Ahmad <khanyousafzaideveloper@gmail.com>
2024-01-15 12:25:49 +0500

    TextField

--- app/src/main/java/com/example/unscramble/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/GameScreen.kt
@@ -1,14 +1,12 @@
 package com.example.unscramble
 
-import android.graphics.Outline
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -16,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,9 +36,8 @@ fun GameScreen(
             var a : Int = 0
             Text(text = " $a / 10" )
             Text(text = currentScrambledWord)
-            Text(text = "Unscamble the words using all letters")
-
-            OutlinedTextField(value = "", onValueChange = { } )
+            Text(text = "Unscramble the words using all letters")
+            OutlinedTextField(value = "", onValueChange = { }, keyboardActions = KeyboardActions(onDone = {  } ) )
         }
         Button(onClick = { /*TODO*/ }) {
             Text(text = "Submit")
